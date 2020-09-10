9ca3be228e63d1651e446e5a59bbbdf8cb0b4ef9
AurelionAustralis <milostod1997@gmail.com>
2020-09-10 19:37:57 +0200

    Databse using EF code first added.

---
+++ SHES/UI/ShesDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Common.SHES_Components;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI
+{
+    public class ShesDbContext : DbContext
+    {
+        public DbSet<Consumer> Consumers { get; set; }
+        public DbSet<SolarPanel> SolarPanels { get; set; }
+        public DbSet<Battery> Battery { get; set; }
+        public ShesDbContext() : base("SHES")
+        {
+
+        }
+
+    }
+}
